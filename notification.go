37924eaf159ffb57d7fbe47b8e78919ab668d5cc
Ben Morgan <neembi@googlemail.com>
2014-01-04 20:08:38 +0100

    Initial draft

---
+++ notification.go
@@ -0,0 +1,86 @@
+// Copyright (c) 2013, Ben Morgan. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
+package notify
+
+import (
+	"errors"
+	"time"
+
+	"github.com/guelfey/go.dbus"
+)
+
+type NotificationUrgency byte
+
+const (
+	LowUrgency NotificationUrgency = iota
+	NormalUrgency
+	CriticalUrgency
+)
+
+func (u NotificationUrgency) asHint() map[string]dbus.Variant {
+	return map[string]dbus.Variant{"hint": dbus.MakeVariant(byte(u))}
+}
+
+type Notification struct {
+	Name    string
+	Summary string
+	Body    string
+
+	IconPath string
+	Timeout  time.Duration
+	Urgency  NotificationUrgency
+}
+
+func New(name, summary, body, icon string, timeout time.Duration, urgency NotificationUrgency) *Notification {
+	return &Notification{name, summary, body, icon, timeout, urgency}
+}
+
+func (n Notification) Send() (id uint32, err error) {
+	return notify(n.Name, n.Summary, n.Body, n.IconPath, 0, nil, n.Urgency.asHint(), n.TimeoutInMS())
+}
+
+func (n Notification) SendMsg(summary, body string) (id uint32, err error) {
+	return notify(n.Name, summary, body, n.IconPath, 0, nil, n.Urgency.asHint(), n.TimeoutInMS())
+}
+
+func (n Notification) SendUrgentMsg(summary, body string, urgency NotificationUrgency) (id uint32, err error) {
+	return notify(n.Name, summary, body, n.IconPath, 0, nil, urgency.asHint(), n.TimeoutInMS())
+}
+
+func (n Notification) Replace(id uint32) error {
+	_, err := notify(n.Name, n.Summary, n.Body, n.IconPath, id, nil, n.Urgency.asHint(), n.TimeoutInMS())
+	return err
+}
+
+func (n Notification) ReplaceMsg(id uint32, summary, body string) error {
+	_, err := notify(n.Name, summary, body, n.IconPath, id, nil, n.Urgency.asHint(), n.TimeoutInMS())
+	return err
+}
+
+func (n Notification) ReplaceUrgentMsg(id uint32, summary, body string, urgency NotificationUrgency) error {
+	_, err := notify(n.Name, summary, body, n.IconPath, id, nil, urgency.asHint(), n.TimeoutInMS())
+	return err
+}
+
+func (n Notification) TimeoutInMS() int32 {
+	return int32(n.Timeout / time.Millisecond)
+}
+
+func notify(name, summary, body, icon string, replacesID uint32, actions []string, hints map[string]dbus.Variant, timeout int32) (id uint32, err error) {
+	conn, err := dbus.SessionBus()
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
+
+	obj := conn.Object("org.freedesktop.Notifications", "/org/freedesktop/Notifications")
+	call := obj.Call("org.freedesktop.Notifications.Notify", 0, name, replacesID, icon, summary, body, actions, hints, timeout)
+	if call.Err != nil {
+		return 0, call.Err
+	} else if call.Store(&id) != nil {
+		return 0, errors.New("unrecognized response from notify daemon")
+	}
+	return
+}
--- notify.go
+++ notify.go
@@ -1,64 +1,49 @@
-package main
+// Copyright (c) 2013, Ben Morgan. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
+// Package notify provides DBus notifications.
+package notify
 
 import (
-	"errors"
-	"fmt"
 	"time"
-
-	"github.com/guelfey/go.dbus"
 )
 
-type Urgency byte
+// note acts as the default notification, which allows you to set default
+// parameters and then send messages without creating any Notifications.
+var note Notification
 
-const (
-	LowUrgency Urgency = iota
-	NormalUrgency
-	CriticalUrgency
-)
-
-// notifications are displayed for the duration of timeout.
-var timeout = 2 * time.Second
-
-func Timeout() time.Duration       { return timeout }
-func SetTimeout(dur time.Duration) { timeout = dur }
-
-func Notify(name, summary, body string) error {
-	return NotifyIcon(name, summary, body, "")
+func Init(name, icon string, timeout time.Duration, urgency NotificationUrgency) {
+	note.Name = name
+	note.IconPath = icon
+	note.Timeout = timeout
+	note.Urgency = urgency
 }
 
-func NotifyUrgency(name, summary, body, icon string, urgency Urgency) error {
-	hints := map[string]dbus.Variant{"urgency": dbus.MakeVariant(byte(urgency))}
-	_, err := notify(name, summary, body, icon, 0, nil, hints, int32(timeout/time.Millisecond))
-	return err
+func Name() string        { return note.Name }
+func SetName(name string) { note.Name = name }
+
+func IconPath() string        { return note.IconPath }
+func SetIconPath(path string) { note.IconPath = path }
+
+func Timeout() time.Duration       { return note.Timeout }
+func SetTimeout(dur time.Duration) { note.Timeout = dur }
+
+func Urgency() NotificationUrgency           { return note.Urgency }
+func SetUrgency(urgency NotificationUrgency) { note.Urgency = urgency }
+
+func SendMsg(summary, body string) (id uint32, err error) {
+	return note.SendMsg(summary, body)
 }
 
-func NotifyIcon(name, summary, body, icon string) error {
-	_, err := notify(name, summary, body, icon, 0, nil, nil, int32(timeout/time.Millisecond))
-	return err
+func SendUrgentMsg(summary, body string, urgency NotificationUrgency) (id uint32, err error) {
+	return note.SendUrgentMsg(summary, body, urgency)
 }
 
-func notify(name, summary, body, icon string, replacesID uint32, actions []string, hints map[string]dbus.Variant, timeout int32) (id uint32, err error) {
-	conn, err := dbus.SessionBus()
-	if err != nil {
-		return 0, err
-	}
-
-	obj := conn.Object("org.freedesktop.Notifications", "/org/freedesktop/Notifications")
-	call := obj.Call("org.freedesktop.Notifications.Notify", 0, name, replacesID, icon, summary, body, actions, hints, timeout)
-	if call.Err != nil {
-		return 0, call.Err
-	} else if call.Store(&id) != nil {
-		return 0, errors.New("unrecognized response from notify daemon")
-	}
-	return
+func ReplaceMsg(id uint32, summary, body string) error {
+	return note.ReplaceMsg(id, summary, body)
 }
 
-func main() {
-	err := Notify("mywire", "Connection OK", "")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	time.Sleep(2 * time.Second)
-	NotifyUrgency("mywire", "Connection BAD", "", "", CriticalUrgency)
+func ReplaceUrgentMsg(id uint32, summary, body string, urgency NotificationUrgency) error {
+	return note.ReplaceUrgentMsg(id, summary, body, urgency)
 }
